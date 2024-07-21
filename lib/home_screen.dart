c98b6dc0e89ca6462fdb1fdc40a83b59a77aad37
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-21 14:29:42 +0000

    updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,11 +17,102 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
-        child: Center(
-          child: Text(
-            'data',
-            style: TextStyle(color: Colors.white),
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/images/sonnet.png",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 40.0),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          height: 115.0,
+                          width: 115.0,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFFFFFFF),
+                            borderRadius: BorderRadius.circular(100.0),
+                            image: const DecorationImage(
+                              image: AssetImage(
+                                "assets/images/sonnetlogo.png",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Column(
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        style: TextStyle(
+                          height: 1.3,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text:
+                                'AI curated music playlist just for your mood \n',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w300,
+                              color: Color(0xFFFFFFFF),
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Get Started Now!',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFFFFFFF),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Container(
+                        height: 50.0,
+                        width: 50.0,
+                        padding: const EdgeInsets.all(2.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFFFFFF),
+                          borderRadius: BorderRadius.circular(100.0),
+                        ),
+                        child: const Center(
+                          child: Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
