5dbe9a808b752d7b2f57862a9783621a29613f75
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-21 12:01:47 +0000

    home updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF330000),
+              Color(0xFF000000),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'data',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
