8fe3940d1d3608e55f09990818a2ed19d3f20d24
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-22 21:10:02 +0000

    updatws

--- lib/prompt_screen.dart
+++ lib/prompt_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class PromptScreen extends StatefulWidget {
   final VoidCallback showHomeScreen;
@@ -23,7 +24,120 @@ class _PromptScreenState extends State<PromptScreen> {
             ],
           ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: RandomCircles(),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Genre',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFFFFFFFF).withOpacity(0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class RandomCircles extends StatelessWidget {
+  final Random random = Random();
+  final List<Map<String, String>> moodData = [
+    {'mood': 'Happy', 'image': 'assets/images/happy.png'},
+    {'mood': 'Heartbroken', 'image': 'assets/images/heartbroken.png'},
+    {'mood': 'Grateful', 'image': 'assets/images/grateful.png'},
+    {'mood': 'Relaxed', 'image': 'assets/images/relaxed.png'},
+    {'mood': 'Anxious', 'image': 'assets/images/anxious.png'},
+    {'mood': 'Romance', 'image': 'assets/images/romance.png'},
+    {'mood': 'Energetic', 'image': 'assets/images/energetic.png'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        List<Widget> circles = [];
+        List<Rect> positions = [];
+
+        for (int i = 0; i < 7; i++) {
+          double size =
+              random.nextDouble() * 100 + 50; // Size between 50 and 150
+          double left, top;
+          Rect newPosition;
+
+          // Ensure circles do not overlap
+          bool doesOverlap;
+          do {
+            left = random.nextDouble() * (constraints.maxWidth - size);
+            top = random.nextDouble() * (constraints.maxHeight - size);
+            newPosition = Rect.fromLTWH(left, top, size, size);
+
+            doesOverlap =
+                positions.any((position) => position.overlaps(newPosition));
+          } while (doesOverlap);
+
+          positions.add(newPosition);
+
+          Color color = Color.fromARGB(
+            255,
+            random.nextInt(256),
+            random.nextInt(256),
+            random.nextInt(256),
+          );
+
+          circles.add(
+            Positioned(
+              left: left,
+              top: top,
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  shape: BoxShape.circle,
+                ),
+                child: Container(
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(
+                    color: color,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        moodData[i]['image']!,
+                        width: size * 0.8,
+                        height: size * 0.8,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+
+        return Stack(children: circles);
+      },
     );
   }
 }
