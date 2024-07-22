11e338878aa2f2a2eafd03785c1b1c66c153dc92
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-22 08:45:19 +0000

    prompt added

---
+++ lib/prompt_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PromptScreen extends StatefulWidget {
+  const PromptScreen({super.key});
+
+  @override
+  State<PromptScreen> createState() => _PromptScreenState();
+}
+
+class _PromptScreenState extends State<PromptScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
